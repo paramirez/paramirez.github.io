e97ba662053b46490a19ba228813634d7e0be064
Pablo Ramirez <p.ramirez@mobiera.com>
2020-08-20 21:52:42 -0500

    Primer commit

---
+++ precache-manifest.34c4fe049091c391070384950f7f84be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610c41794e49c7b3ad70",
+    "url": "/css/app.a0a25f36.css"
+  },
+  {
+    "revision": "8af1f4783d5b9ec1838371ad42fb7a3b",
+    "url": "/img/cv.8af1f478.png"
+  },
+  {
+    "revision": "93811222f0bc15ef612fa4875d16aae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f6fa9739a37841e680a",
+    "url": "/js/about.821689fc.js"
+  },
+  {
+    "revision": "610c41794e49c7b3ad70",
+    "url": "/js/app.6c65b4ed.js"
+  },
+  {
+    "revision": "7522c0720069a6f47515",
+    "url": "/js/chunk-vendors.ce061d87.js"
+  },
+  {
+    "revision": "899ac59d9a58805378490b288f06c3ca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
